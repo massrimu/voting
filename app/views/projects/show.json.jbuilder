a9fd5894022816a5852d30eab1ac85a9a995330f
John Monberg <john.monberg@gmail.com>
2016-02-28 20:47:08 -0500

    Add project

---
+++ app/views/projects/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @project, :id, :title, :description, :image, :webpage, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  resources :projects
+
   resources :books
 
   devise_for :users
 
   get 'welcome/index'
 
-  authenticated :user do |variable|
-    root 'dashboard#index', as: 'authenticated_root'
-  end
+#  authenticated :user do |variable|
+#    root 'dashboard#index', as: 'authenticated_root'
+#  end
 
   root 'welcome#index'
 
