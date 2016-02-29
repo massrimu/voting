89e2bb020755bbcffa5a091cb6ea04f284fc1ff0
John Monberg <john.monberg@gmail.com>
2016-02-29 17:02:46 -0500

    Add has many projects to user

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,10 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  include Devise::TestHelpers
+end
+
+class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
